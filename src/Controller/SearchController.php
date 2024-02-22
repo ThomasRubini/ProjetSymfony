c838a3d2edcb44a5ea68b854743c61c197da93e5
ZeKap <kapismath@gmail.com>
2024-02-22 09:05:55 +0100

    renamed 'search' to 'app_search' in router

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -11,7 +11,8 @@ use GuzzleHttp\Client;
 class SearchController extends AbstractController
 {
 
-    private static function makeRequest(string $method,string $endpoint, array $querryParams){
+    private static function makeRequest(string $method, string $endpoint, array $querryParams)
+    {
 
         // Create a new Guzzle HTTP client
         $baseUri = 'https://api.discogs.com/';
@@ -19,35 +20,35 @@ class SearchController extends AbstractController
             'base_uri' => $baseUri,
             'timeout'  => 2.0, // You can set request timeout (in seconds)
         ]);
-        
+
         // Set the base URI for the request and any default request options
         $auth = array(
             "key" => "kLzxeHuJCXRyQuBURaEN",
             "secret" => "biDCkuoMbGLeDtQGqCPCtbVqmzPaVmQG"
         );
-        
+
         // Create the query parameters array
         $querryParams = array_merge($querryParams, $auth);
-        
+
         // Send a GET request to the specified URI with the query parameters
-        $response = $client->request($method, "/database/".$endpoint, [
+        $response = $client->request($method, "/database/" . $endpoint, [
             'query' => $querryParams
         ]);
-    
+
         // Get the status code of the response
         $statusCode = $response->getStatusCode();
-    
+
         // Get the body of the response
         $body = $response->getBody()->getContents();
 
-        return json_decode($body,true);
+        return json_decode($body, true);
     }
-    
-    #[Route('/search', name: 'search')]
+
+    #[Route('/search', name: 'app_search')]
     public function index(Request $request): Response
     {
         $page = $request->query->get('page');
-        $result = SearchController::makeRequest("GET","search",[
+        $result = SearchController::makeRequest("GET", "search", [
             "q" => $request->query->get('q'),
             "type" => "release",
             "page" => strval($page),
@@ -57,9 +58,9 @@ class SearchController extends AbstractController
         return $this->render('search/search.html.twig', [
             'controller_name' => 'SearchController',
             'query' => $request->query->get('q'),
-            'page' => $request->query->get('page'), 
+            'page' => $request->query->get('page'),
             "next_page" => $page < $result["pagination"]["pages"] ? strval($page + 1) : strval($page),
-            "previous_page" => $page >1 ? strval($page - 1) : strval($page),
+            "previous_page" => $page > 1 ? strval($page - 1) : strval($page),
             'results' => $result["results"]
         ]);
     }
