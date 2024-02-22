2aa986ab85e24b2399e3f9629a77dddb433da5ef
Djalim Simaila <djalimS.pro@outlook.fr>
2024-02-22 09:00:22 +0000

    added userfavorites

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
@@ -28,6 +30,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     #[ORM\Column]
     private ?string $password = null;
+
+    #[ORM\ManyToMany(targetEntity: Album::class)]
+    private Collection $liked;
+
+    public function __construct()
+    {
+        $this->liked = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -97,5 +107,29 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    /**
+     * @return Collection<int, Album>
+     */
+    public function getLiked(): Collection
+    {
+        return $this->liked;
+    }
+
+    public function addLiked(Album $liked): static
+    {
+        if (!$this->liked->contains($liked)) {
+            $this->liked->add($liked);
+        }
+
+        return $this;
+    }
+
+    public function removeLiked(Album $liked): static
+    {
+        $this->liked->removeElement($liked);
+
+        return $this;
     }
 }
